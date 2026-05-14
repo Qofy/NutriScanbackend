25964568c0960b029b92f33c64879dc5af14d0a6
Qofy <safopencilart@icloud.com>
2026-05-14 23:20:31 +0200

    Add PostgreSQL support with DATABASE_URL env var

--- nutriscan/settings.py
+++ nutriscan/settings.py
@@ -61,12 +61,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'nutriscan.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+import dj_database_url
+
+if os.getenv('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.config(default=os.getenv('DATABASE_URL'), conn_max_age=600)
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
