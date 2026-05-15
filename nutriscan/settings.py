5a24ddfb44003889c2d104a4f5f4664ca3455d6a
Qofy <safopencilart@icloud.com>
2026-05-16 00:34:52 +0200

    fix: Add nutriscan.ink to CORS allowed origins

--- nutriscan/settings.py
+++ nutriscan/settings.py
@@ -92,6 +92,8 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:8000',
     'https://nutri-scan-murex.vercel.app',
     'https://nutri-scan-git-main-safo-kofi-agyekums-projects.vercel.app',
+    'https://nutriscan.ink',
+    'http://nutriscan.ink',
 ]
 
 CORS_ALLOW_CREDENTIALS = True
